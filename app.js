1069a319455d309d69cb86abaee2614382ba8ac8
Jose Arguello <johao5991@gmail.com>
2019-09-11 17:13:24 -0400

    Modulo de Events

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const fs = require("fs");
+const EventEmitter = require("events"); //Defining a class
+const emitter = new EventEmitter(); // Instantiating the class
 
-fs.readdir("./", (err, files) => {
-  if (err) throw err;
-  console.log(files);
+// Register a listener
+emitter.on("messageLogged", arg => {
+  console.log("Listener called");
+  console.log(arg.id);
+  console.log(arg.url);
 });
+
+// Raise an event
+emitter.emit("messageLogged", { id: 1, url: "http://" }); //Signaling that an event had happened
